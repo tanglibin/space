a48f964bb07f195ad5dcd099a15a97264d7d1a51
tanglibin <tanglibin>
2018-11-13 15:13:10 +0800

    login static

---
+++ themes/admin/src/page/Login.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="login-page">
+        <div class="content">
+            <h2>管理后台登录</h2>
+            <div class="enter-item">
+                <i class="icon-user"></i>
+                <input placeholder="请输入账号">
+            </div>
+            <div class="enter-item">
+                <i class="icon-password"></i>
+                <input placeholder="请输入账号">
+            </div>
+            <div class="login-btn">登 录</div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Common from '../assets/js/common';
+
+export default {
+    name: "login",
+    data() {
+        return {
+            
+        };
+    },
+    methods: {
+        
+    }
+};
+</script>
--- themes/admin/src/router/index.js
+++ themes/admin/src/router/index.js
@@ -5,6 +5,8 @@ import Router from 'vue-router'
 import Error from '@/components/Error'
 //管理后台容器页面
 import Container from '@/components/Container'
+//登录
+import Login from '@/page/Login'
 //首页
 import Home from '@/page/Home'
 //AK管理
@@ -38,10 +40,7 @@ export default new Router({
                 { path: 'ak', component: Ak, name: "AK管理" },
                 { path: 'user', component: User, name: "用户管理" },
                 { path: 'category', component: Category, name: "标签管理" },
-                {
-                    path: 'journal',
-                    component: Journal,
-                    redirect: '/',
+                { path: 'journal', component: Journal, redirect: '/',
                     children: [
                         { path: '/', component: JournalList, name: "日志管理" },
                         { path: 'add', component: JournalUpdate, name: "新增日志" },
@@ -52,6 +51,11 @@ export default new Router({
                 { path: 'push', component: Push, name: "好文推荐" },
                 { path: 'download', component: Download, name: "资源下载" }
             ]
+        },
+        {
+            path: '/login',
+            component: Login,
+            name: "登录"
         },
         {
             path: '*',
