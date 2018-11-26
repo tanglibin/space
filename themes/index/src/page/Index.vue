b37c6d209cd055b5b4e08162dc90eea1fea8f511
tanglibin <tanglibin>
2018-11-26 19:02:23 +0800

    .

--- themes/index/src/page/Index.vue
+++ themes/index/src/page/Index.vue
@@ -22,6 +22,24 @@
                             <div class="item-summary">如果你正在寻找一个新的CSS框架为你的下一个前端开发项目，请阅读一个开发人员对一些流行的选择的评论。 当建立一个网站时，使用CSS框架是一个真正的节省时间的方法，..</div>
                             <el-button round size="mini" class="item-btn">阅读全文 »</el-button>
                         </li>
+                        <li class="writing-item">
+                            <p class="item-title">2018年十大轻量级的CSS框架为构建快速网站</p>
+                            <p class="item-meta">发布时间: 018-09-25   |   标签：CSS3 </p>
+                            <div class="item-summary">如果你正在寻找一个新的CSS框架为你的下一个前端开发项目，请阅读一个开发人员对一些流行的选择的评论。 当建立一个网站时，使用CSS框架是一个真正的节省时间的方法，..</div>
+                            <el-button round size="mini" class="item-btn">阅读全文 »</el-button>
+                        </li>
+                        <li class="writing-item">
+                            <p class="item-title">2018年十大轻量级的CSS框架为构建快速网站</p>
+                            <p class="item-meta">发布时间: 018-09-25   |   标签：CSS3 </p>
+                            <div class="item-summary">如果你正在寻找一个新的CSS框架为你的下一个前端开发项目，请阅读一个开发人员对一些流行的选择的评论。 当建立一个网站时，使用CSS框架是一个真正的节省时间的方法，..</div>
+                            <el-button round size="mini" class="item-btn">阅读全文 »</el-button>
+                        </li>
+                        <li class="writing-item">
+                            <p class="item-title">2018年十大轻量级的CSS框架为构建快速网站</p>
+                            <p class="item-meta">发布时间: 018-09-25   |   标签：CSS3 </p>
+                            <div class="item-summary">如果你正在寻找一个新的CSS框架为你的下一个前端开发项目，请阅读一个开发人员对一些流行的选择的评论。 当建立一个网站时，使用CSS框架是一个真正的节省时间的方法，..</div>
+                            <el-button round size="mini" class="item-btn">阅读全文 »</el-button>
+                        </li>
                     </ul>
                 </section>
 
@@ -32,6 +50,24 @@
                         <router-link to="/recommend" class="show-more">查看更多<i class="el-icon-d-arrow-right"></i></router-link>
                     </div>
                     <ul>
+                        <li class="writing-item">
+                            <p class="item-title">2018年十大轻量级的CSS框架为构建快速网站</p>
+                            <p class="item-meta">发布时间: 018-09-25   |   标签：CSS3 </p>
+                            <div class="item-summary">如果你正在寻找一个新的CSS框架为你的下一个前端开发项目，请阅读一个开发人员对一些流行的选择的评论。如果你正在寻找一个新的CSS框架为你的下一个前端开发项目，请阅读一个开发人员对一些流行的选择的评论。如果你正在寻找一个新的CSS框架为你的下一个前端开发项目，请阅读一个开发人员对一些流行的选择的评论。 当建立一个网站时，使用CSS框架是一个真正的节省时间的方法，..</div>
+                            <el-button round size="mini" class="item-btn">阅读全文 »</el-button>
+                        </li>
+                        <li class="writing-item">
+                            <p class="item-title">2018年十大轻量级的CSS框架为构建快速网站</p>
+                            <p class="item-meta">发布时间: 018-09-25   |   标签：CSS3 </p>
+                            <div class="item-summary">如果你正在寻找一个新的CSS框架为你的下一个前端开发项目，请阅读一个开发人员对一些流行的选择的评论。如果你正在寻找一个新的CSS框架为你的下一个前端开发项目，请阅读一个开发人员对一些流行的选择的评论。如果你正在寻找一个新的CSS框架为你的下一个前端开发项目，请阅读一个开发人员对一些流行的选择的评论。 当建立一个网站时，使用CSS框架是一个真正的节省时间的方法，..</div>
+                            <el-button round size="mini" class="item-btn">阅读全文 »</el-button>
+                        </li>
+                        <li class="writing-item">
+                            <p class="item-title">2018年十大轻量级的CSS框架为构建快速网站</p>
+                            <p class="item-meta">发布时间: 018-09-25   |   标签：CSS3 </p>
+                            <div class="item-summary">如果你正在寻找一个新的CSS框架为你的下一个前端开发项目，请阅读一个开发人员对一些流行的选择的评论。如果你正在寻找一个新的CSS框架为你的下一个前端开发项目，请阅读一个开发人员对一些流行的选择的评论。如果你正在寻找一个新的CSS框架为你的下一个前端开发项目，请阅读一个开发人员对一些流行的选择的评论。 当建立一个网站时，使用CSS框架是一个真正的节省时间的方法，..</div>
+                            <el-button round size="mini" class="item-btn">阅读全文 »</el-button>
+                        </li>
                         <li class="writing-item">
                             <p class="item-title">2018年十大轻量级的CSS框架为构建快速网站</p>
                             <p class="item-meta">发布时间: 018-09-25   |   标签：CSS3 </p>
@@ -70,10 +106,38 @@
                     </ul>
                 </section>
                 <section class="news-box">
-                    新闻资讯
+                    <div class="box-title">
+                        <span class="title-text">新闻资讯</span>
+                    </div>
+                    <ul>
+                        <li class="link-item">学习nodejs：express 入门和基础知识学习nodejs：express 入门和基础知识学习nodejs：express 入门和基础知识</li>
+                        <li class="link-item">学习nodejs：express 入门和基础知识</li>
+                        <li class="link-item">学习nodejs：express 入门和基础知识</li>
+                        <li class="link-item">学习nodejs：express 入门和基础知识</li>
+                        <li class="link-item">学习nodejs：express 入门和基础知识</li>
+                        <li class="link-item">学习nodejs：express 入门和基础知识</li>
+                        <li class="link-item">学习nodejs：express 入门和基础知识</li>
+                        <li class="link-item">学习nodejs：express 入门和基础知识</li>
+                        <li class="link-item">学习nodejs：express 入门和基础知识</li>
+                        <li class="link-item">学习nodejs：express 入门和基础知识</li>
+                    </ul>
                 </section>
                 <section class="resource-box">
-                    资源下载
+                    <div class="box-title">
+                        <span class="title-text">资源下载</span>
+                    </div>
+                    <ul>
+                        <li class="resource-item"><i style="background-image:url(/upload/css.png)"></i><p>css API</p></li>
+                        <li class="resource-item">学习nodejs：express 入门和基础知识</li>
+                        <li class="resource-item">学习nodejs：express 入门和基础知识</li>
+                        <li class="resource-item">学习nodejs：express 入门和基础知识</li>
+                        <li class="resource-item">学习nodejs：express 入门和基础知识</li>
+                        <li class="resource-item">学习nodejs：express 入门和基础知识</li>
+                        <li class="resource-item">学习nodejs：express 入门和基础知识</li>
+                        <li class="resource-item">学习nodejs：express 入门和基础知识</li>
+                        <li class="resource-item">学习nodejs：express 入门和基础知识</li>
+                        <li class="resource-item">学习nodejs：express 入门和基础知识</li>
+                    </ul>
                 </section>
             </div>
         </div>
